0774d72e74a8b46ab81c124b9f20eeafef13bb25
ifty03 <ashikulislamb84@gmail.com>
2025-04-28 10:46:19 +0600

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="">
+     <h2 className='text-red-500'>this is my project m360ict</h2>
+    </div>
+  )
+}
+
+export default App
