a39104832b3e9fc90435635cc745e0900f3c88d8
ifty03 <ashikulislamb84@gmail.com>
2025-04-30 00:12:15 +0600

    setup redux tolkit and RTK query

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
+import Counter from './Components/Counter'
 
 function App() {
 
   return (
     <div className="">
      <h2 className='text-red-500'>this is my project m360ict</h2>
+
+
+     <Counter/>
     </div>
   )
 }
